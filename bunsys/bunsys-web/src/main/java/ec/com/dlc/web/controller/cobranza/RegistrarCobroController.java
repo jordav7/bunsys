194587210e2aba9f04378a9fadcffe1efcae9a72
jordav.7 <jordav.7@gmail.com>
2015-03-06 02:56:00 +0000

    x

--- bunsys/bunsys-web/src/main/java/ec/com/dlc/web/controller/cobranza/RegistrarCobroController.java
+++ bunsys/bunsys-web/src/main/java/ec/com/dlc/web/controller/cobranza/RegistrarCobroController.java
@@ -1,8 +1,11 @@
 package ec.com.dlc.web.controller.cobranza;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Date;
+import java.util.List;
+import java.util.Map;
 
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
@@ -12,6 +15,7 @@ import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 
 import org.primefaces.context.RequestContext;
+import org.primefaces.extensions.util.MessageUtils;
 
 import ec.com.dlc.bunsys.entity.administracion.Tadmcatalogo;
 import ec.com.dlc.bunsys.entity.facturacion.Tfaccabfactura;
@@ -20,9 +24,11 @@ import ec.com.dlc.bunsys.entity.facturacion.Tfaccuentasxcobrar;
 import ec.com.dlc.bunsys.entity.seguridad.Tsyspersona;
 import ec.com.dlc.bunsys.facade.BunsysService;
 import ec.com.dlc.bunsys.util.FacturacionException;
+import ec.com.dlc.web.commons.resource.ContenidoMessages;
 import ec.com.dlc.web.controller.base.BaseController;
 import ec.com.dlc.web.datamanager.base.BaseDatamanager;
 import ec.com.dlc.web.datamanager.cobranza.RegistrarCobroDatamanager;
+import ec.com.dlc.web.util.jsf.MessagesUtil;
 
 @ManagedBean
 @ViewScoped
@@ -47,6 +53,7 @@ public class RegistrarCobroController extends BaseController{
 		regCobroDatamanager.getCxcRegistro().getTfaccliente().setTsyspersona(new Tsyspersona());
 		regCobroDatamanager.getCxcRegistro().setTadmestado(new Tadmcatalogo());
 		regCobroDatamanager.getCxcRegistro().setFechapago(new Date());
+		regCobroDatamanager.setSelCxc(false);
 	}
 	
 	public void buscar() {
@@ -55,27 +62,46 @@ public class RegistrarCobroController extends BaseController{
 		regCobroDatamanager.getCxcRegistro().getTfaccliente().getTsyspersona().setNombres(clientes.getTsyspersona().getNombres());
 		regCobroDatamanager.setCxcPendientesColl(bunsysService.obtenerCuentasPorCobrar(regCobroDatamanager.getLoginDatamanager().getLogin().getPk().getCodigocompania(), 
 				clientes.getPk().getCodigocliente()));
-		
 		regCobroDatamanager.setTotal(BigDecimal.ZERO);
-	}
-	
-	private void chequearDetalle(){
-		BigDecimal total = BigDecimal.ZERO, saldo =BigDecimal.ZERO,  signo = BigDecimal.ONE;
+		regCobroDatamanager.setSelCxc(false);
 		
 		for (Tfaccuentasxcobrar cxc : regCobroDatamanager.getCxcPendientesColl()) {
-			if(cxc.getValor().compareTo(BigDecimal.ZERO)>0 ){
-				saldo =cxc.getSaldo().subtract(cxc.getValor());
-				
-				if(saldo.compareTo(BigDecimal.ZERO) >= 0){
-					cxc.setSaldo(saldo);
-				}else{
-					throw new FacturacionException("El valor de la cuenta por cobrar no puede ser mayor a la deuda total");
-				}
+			cxc.setValor(cxc.getSaldo());
+			cxc.addAditionalProperty("saldoCxc", cxc.getSaldo());
+			cxc.addAditionalProperty("editCxc", true);
+			cxc.addAditionalProperty("seleccionado", false);
+		}
+	}
+	
+	private void selectedCxc(Tfaccuentasxcobrar cxc){
+		BigDecimal saldo =BigDecimal.ZERO;
+		if(cxc.getValor().compareTo(BigDecimal.ZERO)>0 ){
+			saldo =cxc.getSaldo().subtract(cxc.getValor());
+			
+			if(saldo.compareTo(cxc.getSaldo()) <= 0 && saldo.compareTo(BigDecimal.ZERO)>= 0){
+				cxc.addAditionalProperty("saldoCxc", saldo);
 			}else{
-				throw new FacturacionException("El valor de la cuenta por cobrar debe ser mayor que cero");
+				cxc.addAditionalProperty("saldoCxc", BigDecimal.ZERO);
+				cxc.setValor(cxc.getSaldo());
+				throw new FacturacionException("El valor de la cuenta por cobrar no puede ser mayor a la deuda total");
 			}
-			signo = (boolean) cxc.getAditionalProperty("seleccionado")?signo:new BigDecimal(-1);
-			total = total.add((cxc.getValor().multiply(signo)));
+		}else{
+			cxc.setValor(cxc.getSaldo());
+			throw new FacturacionException("El valor de la cuenta por cobrar debe ser mayor que cero");
+		}
+	}
+	
+	private void unselectedCxc(Tfaccuentasxcobrar cxc) {
+		cxc.addAditionalProperty("saldoCxc", cxc.getSaldo());
+		cxc.setValor(cxc.getSaldo());
+	}
+	
+	private void totalizar() {
+		BigDecimal total = BigDecimal.ZERO;
+		for (Tfaccuentasxcobrar cxc : regCobroDatamanager.getCxcPendientesColl()) {
+			if((boolean) cxc.getAditionalProperty("seleccionado")){
+				total = total.add(cxc.getValor());
+			}
 		}
 		
 		regCobroDatamanager.setTotal(total);
@@ -84,42 +110,73 @@ public class RegistrarCobroController extends BaseController{
 	public void seleccionarTodos() {
 		try{
 			for (Tfaccuentasxcobrar cxc : regCobroDatamanager.getCxcPendientesColl()) {
-				cxc.addAditionalProperty("seleccionado", regCobroDatamanager.isSelCxc());
+				if(regCobroDatamanager.isSelCxc()){
+					cxc.addAditionalProperty("editCxc", false);
+					cxc.addAditionalProperty("seleccionado", true);
+					this.selectedCxc(cxc);
+				}else{
+					cxc.addAditionalProperty("editCxc", true);
+					cxc.addAditionalProperty("seleccionado", false);
+					this.unselectedCxc(cxc);
+				}
 			}
 			
-			if(regCobroDatamanager.isSelCxc()){
-				this.chequearDetalle();
+			this.totalizar();
+			
+    	}catch(Throwable e){
+    		MessagesUtil.showErrorMessage(e.getMessage());
+    	}
+		
+	}
+	
+	public void selectedRegCxc(Tfaccuentasxcobrar cxcPend) {
+		try{
+			if((boolean) cxcPend.getAditionalProperty("seleccionado")){
+				cxcPend.addAditionalProperty("editCxc", false);
+				this.selectedCxc(cxcPend);
 			}else{
-				regCobroDatamanager.setTotal(BigDecimal.ZERO);
+				cxcPend.addAditionalProperty("editCxc", true);
+				this.unselectedCxc(cxcPend);
 			}
 			
+			this.totalizar();
     	}catch(Throwable e){
-    		FacesContext.getCurrentInstance().validationFailed();
-    		RequestContext.getCurrentInstance().showMessageInDialog(new FacesMessage(FacesMessage.SEVERITY_ERROR, e.getMessage(), e.getMessage()));
+    		e.printStackTrace();
+    		MessagesUtil.showErrorMessage(e.getMessage());
     	}
 		
 	}
 	
-	public void seleccionarCxc() {
+	public void editValor(Tfaccuentasxcobrar cxc) {
 		try{
-			this.chequearDetalle();
+			this.selectedRegCxc(cxc);
     	}catch(Throwable e){
-    		FacesContext.getCurrentInstance().validationFailed();
-    		RequestContext.getCurrentInstance().showMessageInDialog(new FacesMessage(FacesMessage.SEVERITY_ERROR, e.getMessage(), e.getMessage()));
+    		MessagesUtil.showErrorMessage(e.getMessage());
     	}
+	}
+	
+	public void guardar() {
+		List<Tfaccuentasxcobrar> cxcCollAb = new ArrayList<Tfaccuentasxcobrar>();
+		try {
+			for (Tfaccuentasxcobrar cxc : regCobroDatamanager.getCxcPendientesColl()) {
+				if((boolean) cxc.getAditionalProperty("seleccionado")){
+					cxc.setSaldo(new BigDecimal(cxc.getAditionalProperty("saldoCxc").toString()));
+					cxcCollAb.add(cxc);
+				}
+			}
+			
+			bunsysService.registrarPago(cxcCollAb, regCobroDatamanager.getLoginDatamanager().getLogin().getPk().getCodigocompania(), 
+					ContenidoMessages.getInteger("cod_catalogo_estado_cuentaxcobrar") , ContenidoMessages.getInteger("cod_tipo_doc_cuentaxcobrar"),
+					regCobroDatamanager.getCxcRegistro().getNumdoc(), regCobroDatamanager.getCxcRegistro().getConcepto(), regCobroDatamanager.getCxcRegistro().getFechapago());
+			
+			MessagesUtil.showErrorMessage("Pago exitoso");
+		} catch (Exception e) {
+			MessagesUtil.showErrorMessage(e.getMessage());
+			e.printStackTrace();
+		}
 		
 	}
 	
-	public void editarValor() {
-		try{
-			this.chequearDetalle();
-    	}catch(Throwable e){
-    		FacesContext.getCurrentInstance().validationFailed();
-    		RequestContext.getCurrentInstance().showMessageInDialog(new FacesMessage(FacesMessage.SEVERITY_ERROR, e.getMessage(), e.getMessage()));
-    	}
-	}
-	
-
 	public RegistrarCobroDatamanager getRegCobroDatamanager() {
 		return regCobroDatamanager;
 	}
